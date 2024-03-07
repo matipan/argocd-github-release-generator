4ba7530a73e941badc43c9c84e4bf253eb9f57f0
Matias Pan <matipan@hey.com>
2024-03-07 10:38:58 -0300

    Return filtered releases based on min_release
    Signed-off-by: Matias Pan <matipan@hey.com>

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21
 require (
 	github.com/99designs/gqlgen v0.17.44
 	github.com/Khan/genqlient v0.7.0
+	github.com/rs/zerolog v1.32.0
 	github.com/vektah/gqlparser/v2 v2.5.11
 	golang.org/x/exp v0.0.0-20240222234643-814bf88cf225
 	golang.org/x/sync v0.6.0
@@ -12,5 +13,9 @@ require (
 
 require (
 	github.com/google/uuid v1.6.0 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sosodev/duration v1.2.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,13 +1,53 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"log"
+	"encoding/json"
 	"net/http"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/pkgerrors"
 )
 
+var (
+	token     = os.Getenv("ARGOCD_TOKEN")
+	githubPat = os.Getenv("GITHUB_PAT")
+	logLevel  = os.Getenv("LOG_LEVEL")
+)
+
+type Request struct {
+	Input Input `json:"input"`
+}
+
+type Input struct {
+	Parameters Parameters `json:"parameters"`
+}
+
+type Parameters struct {
+	Repository string `json:"repository"`
+	MinRelease string `json:"min_release"`
+}
+
+type Output struct {
+	Parameters []Release `json:"parameters"`
+}
+
 func main() {
+	zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
+	l := zerolog.New(os.Stderr).With().Timestamp().Caller().Logger()
+
+	level, err := zerolog.ParseLevel(logLevel)
+	if err != nil {
+		l.Warn().Err(err).Msg("failed to parse log level, defaulting to info")
+		l = l.Level(zerolog.InfoLevel)
+	} else {
+		l = l.Level(level)
+	}
+
+	if githubPat == "" {
+		l.Info().Msg("GITHUB_PATH is not set, private repositories will not be accessible")
+	}
+
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("/api/v1/getparams.execute", func(w http.ResponseWriter, r *http.Request) {
@@ -17,20 +57,85 @@ func main() {
 		}
 
 		// TODO: check for credentials
-
-		fmt.Println("receiving a request")
-		b, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		authz := r.Header.Get("Authorization")
+		if authz != "Bearer "+token {
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
 			return
 		}
 
-		fmt.Printf("body: %s\n", string(b))
+		req := &Request{}
+		if err := json.NewDecoder(r.Body).Decode(req); err != nil {
+			l.Error().Err(err).Msg("failed to decode request body")
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+
+		l.Debug().Msgf("fetching releases for %s", req.Input.Parameters.Repository)
+		releases, err := getReleases(req.Input.Parameters.Repository)
+		if err != nil {
+			l.Error().Err(err).Msg("failed to fetch releases")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		l.Debug().Msgf("fetched %d releases", len(releases))
+
+		out := Output{
+			Parameters: getFilteredReleases(releases, req.Input.Parameters.MinRelease),
+		}
+
+		l.Debug().Msgf("returning %d releases after filtering with min_release of %s", len(out.Parameters), req.Input.Parameters.MinRelease)
 
 		w.Header().Set("Content-Type", "application/json")
+		if err := json.NewEncoder(w).Encode(out); err != nil {
+			l.Error().Err(err).Msg("failed to encode response")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
 		w.WriteHeader(http.StatusOK)
-		w.Write([]byte(`{"output": {"parameters": [{"hello":"world"}]}}`))
 	})
 
-	log.Println(http.ListenAndServe(":8080", mux))
+	l.Println(http.ListenAndServe(":8080", mux))
+}
+
+type Release struct {
+	Name   string `json:"name"`
+	Commit Commit `json:"commit"`
+	NodeID string `json:"node_id"`
+}
+
+type Commit struct {
+	Sha string `json:"sha"`
+	URL string `json:"url"`
+}
+
+func getFilteredReleases(releases []Release, minRelease string) []Release {
+	var filteredReleases []Release
+	for _, r := range releases {
+		if r.Name > minRelease {
+			filteredReleases = append(filteredReleases, r)
+		}
+	}
+
+	return filteredReleases
+}
+
+func getReleases(repo string) ([]Release, error) {
+	rr, err := http.NewRequest(http.MethodGet, "https://api.github.com/repos/"+repo+"/tags", nil)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := http.DefaultClient.Do(rr)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	var releases []Release
+	if err := json.NewDecoder(res.Body).Decode(&releases); err != nil {
+		return nil, err
+	}
+
+	return releases, nil
 }
